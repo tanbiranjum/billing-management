131f03b77c3a855de278f6604fe984f47b5a2935
tanbiranjum <tanbiranjum@hotmail.com>
2021-11-09 17:36:33 +0600

    sidebar added & added animation to sidebar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,63 @@
+import { useState } from 'react'
+import { Box, Button, Collapsible, Heading, Grommet } from 'grommet'
+import { Notification } from 'grommet-icons'
 
+const theme = {
+  global: {
+    colors: {
+      brand: '#228BE6',
+    },
+    font: {
+      family: 'Roboto',
+      size: '28px',
+      height: '20px',
+    },
+  },
+}
 
-function AppBar() {
+const AppBar = (props) => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation="medium"
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+)
+
+function App() {
+  const [showSidebar, setShowSidebar] = useState(false)
   return (
-
+    <Grommet theme={theme} full>
+      <Box fill>
+        <AppBar>
+          <Heading level="3" margin="none">
+            My App
+          </Heading>
+          <Button
+            icon={<Notification />}
+            onClick={() => {
+              setShowSidebar(!showSidebar)
+            }}
+          />
+        </AppBar>
+        <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
+          <Box flex align="center" justify="center">
+            app body
+          </Box>
+          <Collapsible direction="horizontal" open={showSidebar}>
+            <Box flex width="medium" background="light-2" elevation="small" align="center" justify="center">
+              sidebar
+            </Box>
+          </Collapsible>
+        </Box>
+      </Box>
+    </Grommet>
   )
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Button variant="contained">Hello World</Button>
-      <header className="App-header">
-        <p>
-          Hi my name is tanvir
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default App
