f8b73896c01cde8a94a4a43892008c8c0e0ffb59
tanbiranjum <tanbiranjum@hotmail.com>
2021-11-10 22:49:38 +0600

    mui theme added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, Typography } from '@mui/material'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi my name is tanvir
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <CssBaseline /> */}
+      <Typography variant="h1">Hello from Me</Typography>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
